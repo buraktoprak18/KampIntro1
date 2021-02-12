2d1e9e8a0c8dabc1f1774d4bfc9aa303a8bbd520
Burak Toprak <tburak181@gmail.com>
2021-02-12 23:51:27 +0300

    .

---
+++ OOP2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Burak        
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "18";
+            musteri1.Adi = "Burak";
+            musteri1.Soyadi = "Toprak";
+            musteri1.TcNo = "118";
+
+            //kodlamaio
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "11";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "121212";
+
+            // Gerçek Müşteri - Tüzel Müşteri 
+            // SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Ekle(musteri4);
+
+         
+
+
+        }
+    }
+}
