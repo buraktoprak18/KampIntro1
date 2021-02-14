3a79ea03b1ef67d561f16d6b34fdccdbb7ab6d5c
Burak Toprak <tburak181@gmail.com>
2021-02-14 18:12:37 +0300

    Update Program.cs

--- GameProject/Program.cs
+++ GameProject/Program.cs
@@ -6,7 +6,7 @@ namespace GameProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.add(new Gamer { Id = 1, BirthYear = 2001, FirstName = "BURAK", LastName = "TOPRAK", IdentityNumber = 12345});
     }
 }
